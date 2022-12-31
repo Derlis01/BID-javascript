d23b78eafff346eab7cca4a1f338455a38546e75
Derlis Cuba <d.erliscuba14@hotmail.com>
2022-12-30 22:47:33 -0300

    [TAREA] sensei

--- main.js
+++ main.js
@@ -17,5 +17,27 @@ class Ninja {
 }
 
 const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.showStats();
+//ninja1.sayName();
+//ninja1.showStats();
+
+
+class Sensei extends Ninja {
+    constructor (name) {
+        super(name)
+        this.health = 200
+        this.speed = 10
+        this.strength = 10
+        this.wisdom = 10
+    }
+    speakWisdom() {
+        super.drinkShake()
+        console.log('Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.')
+    }
+}
+
+// ejemplo de salida
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
